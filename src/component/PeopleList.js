ff7a0192e7c545db9736f6d8ceaddc7ea285d9f5
Rodrigo de Oliveira <rodrigodeoliveira397@gmail.com>
2020-11-12 13:47:33 -0300

    Fix PeopleDetailsPage bug where it wouldn't update its state

--- src/component/PeopleList.js
+++ src/component/PeopleList.js
@@ -1,52 +1,56 @@
 import React from 'react'
-import {View, StyleSheet, SafeAreaView, FlatList, Text} from 'react-native'
+import { View, StyleSheet, SafeAreaView, FlatList, Text } from 'react-native'
 import PeopleListItem from './PeopleListItem'
 
-const PeopleList = props => {
-    const {peoples, onPressItem} = props   
-    const renderItem = ({item}) => {
-        console.log('AQUI', item)
-        return (
-            <PeopleListItem 
-            key = {item.nome.name}
-            people = {item}
-            onPressItemDetails = {onPressItem}
-            />
-        )
-    }
-    const header= () => {
-        return (
-            <View style={style.headerStyle}>
-                <Text style={style.titleStyle}>
-                    Perfil
-                </Text>
-            </View>
-        )
-    }
-    return(
-        <View style = {style.container}>
-            <SafeAreaView>
-                <FlatList
-                    data={peoples}
-                    renderItem={renderItem}
-                    keyExtractor={(item) => item.nome.name}
-                    ListHeaderComponent={header}
-                />
-            </SafeAreaView>
-        </View>
-    )
+const PeopleList = (props) => {
+	const { peoples, onPressItem } = props
+	const renderItem = ({ item }) => {
+		//console.log('AQUI', item)
+		return (
+			<PeopleListItem
+				key={item.nome.name}
+				people={item}
+				onPressItemDetails={onPressItem}
+			/>
+		)
+	}
+	const header = () => {
+		return (
+			<View style={style.headerStyle}>
+				<Text style={style.titleStyle}>Perfil</Text>
+			</View>
+		)
+	}
+	return (
+		<View style={style.container}>
+			<SafeAreaView>
+				<FlatList
+					data={peoples}
+					renderItem={renderItem}
+					keyExtractor={(item) => item.nome.name}
+					ListHeaderComponent={header}
+				/>
+			</SafeAreaView>
+		</View>
+	)
 }
-const style = StyleSheet.create(
-    {
-        container: {
-            backgroundColor: '#e67300', 
-        },
-        headerStyle: {
-             backgroundColor: "#fff", justifyContent: "center", alignItems: 'center'
-        },
-        titleStyle: {
-            color: '#000', fontSize: 25, fontWeight: "bold", height: 50, justifyContent: "center", alignItems: "center", marginTop: 15
-        }
-    }
-)
+const style = StyleSheet.create({
+	container: {
+		backgroundColor: '#e67300'
+	},
+	headerStyle: {
+		backgroundColor: '#fff',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	titleStyle: {
+		color: '#000',
+		fontSize: 25,
+		fontWeight: 'bold',
+		height: 50,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 15
+	}
+})
 export default PeopleList
--- src/pages/PeopleDetailsPage.js
+++ src/pages/PeopleDetailsPage.js
@@ -1,57 +1,97 @@
 import React from 'react'
-import {ScrollView, Text, StyleSheet,Image, SafeAreaView, View, Dimensions} from 'react-native'
+import {
+	ScrollView,
+	Text,
+	StyleSheet,
+	Image,
+	SafeAreaView,
+	View,
+	Dimensions
+} from 'react-native'
 
-let ScreenHeight = Dimensions.get("window").height;
-export default class PeopleDetailsPage extends React.Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-      console.log(props.route.params.people)
-        return(
-            <ScrollView style = {style.scroll}>
-            <SafeAreaView>
-            <View style = {style.container}>
-              <Image style = {style.picture} source = {{uri: props.route.params.people.imagem}} />
-              <Text style = {style.title}>Nome e sobrenome:</Text>
-              <Text style = {style.text}>{props.route.params.people.nome.name}</Text>
-              <Text style = {style.title}>Gênero: </Text>
-              <Text style = {style.text}> {props.route.params.people.genero}</Text>
-              <Text style = {style.title}>Email:</Text>
-              <Text style = {style.text}>{props.route.params.people.email}</Text>
-              <Text style = {style.title}>Aniversário:</Text>
-              <Text style = {style.text}> {props.route.params.people.aniversario}</Text>
-              <Text style = {style.title}>Idade:</Text>
-              <Text style = {style.text}>{props.route.params.people.idade}</Text>
-            </View>
-            </SafeAreaView>
-            </ScrollView>
-        )
-    }
+let ScreenHeight = Dimensions.get('window').height
+
+export default class PeopleDetailsPage extends React.Component {
+	constructor(props) {
+		super(props)
+		console.log('Passou pelo constructor PDP')
+	}
+
+	render() {
+		const people = this.props.route.params.people
+		//console.log(people)
+		return (
+			<ScrollView style={style.scroll}>
+				<SafeAreaView>
+					<View style={style.container}>
+						<Image
+							style={style.picture}
+							source={{ uri: people.imagem }}
+						/>
+						<Text style={style.title}>Nome e sobrenome:</Text>
+						<Text style={style.text}>{people.nome.name}</Text>
+						<Text style={style.title}>Gênero: </Text>
+						<Text style={style.text}> {people.genero}</Text>
+						<Text style={style.title}>Email:</Text>
+						<Text style={style.text}>{people.email}</Text>
+						<Text style={style.title}>Aniversário:</Text>
+						<Text style={style.text}> {people.aniversario}</Text>
+						<Text style={style.title}>Idade:</Text>
+						<Text style={style.text}>{people.idade}</Text>
+					</View>
+				</SafeAreaView>
+			</ScrollView>
+		)
+	}
 }
-const style = StyleSheet.create(
-  {
-    scroll: {
-      flex: 1,
-      backgroundColor: '#FFF',  
-    },
+const style = StyleSheet.create({
+	scroll: {
+		flex: 1,
+		backgroundColor: '#FFF'
+	},
 
-    line: {
-      height: 100, borderBottomWidth: 1, borderBottomColor: '#000', alignItems: 'center', flexDirection: 'row'
-    },
+	line: {
+		height: 100,
+		borderBottomWidth: 1,
+		borderBottomColor: '#000',
+		alignItems: 'center',
+		flexDirection: 'row'
+	},
 
-    text: {
-      fontFamily: "Arial", fontSize: 25, textAlign: "center", display: "flex" ,flexWrap: 'wrap', justifyContent: 'center'
-    },
+	text: {
+		fontFamily: 'Arial',
+		fontSize: 25,
+		textAlign: 'center',
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'center'
+	},
 
-    picture: {
-      aspectRatio: 1, margin: 40, height: 256, width: 256, justifyContent: 'center', display: 'flex', alignItems: "center"
-    },
-    title: {
-      fontFamily: "Arial", fontSize: 30, textAlign: "center", display: "flex" ,flexWrap: 'wrap', justifyContent: 'center', fontWeight: 'bold', marginBottom: 10, marginTop: 10
-    },
-    container:{
-        justifyContent: 'center', alignItems: 'center', backgroundColor: '#f2f2f2', flex: 1, height: ScreenHeight
-    }
-  }
-)
+	picture: {
+		aspectRatio: 1,
+		margin: 40,
+		height: 256,
+		width: 256,
+		justifyContent: 'center',
+		display: 'flex',
+		alignItems: 'center'
+	},
+	title: {
+		fontFamily: 'Arial',
+		fontSize: 30,
+		textAlign: 'center',
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'center',
+		fontWeight: 'bold',
+		marginBottom: 10,
+		marginTop: 10
+	},
+	container: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#f2f2f2',
+		flex: 1,
+		height: ScreenHeight
+	}
+})
